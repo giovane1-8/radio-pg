dfe4aef9c8e26ced8ac160abe18b5f3e2fc583e6
giovane1-8 <giovane.gi2012@hotmail.com>
2023-01-23 18:40:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import "./App.css"
+import axios from "axios"
+
+function App() {
+
+
+  return (
+    <>
+      <label>
+        Assunto:
+        <input type="text" id=''/>
+      </label>
+      <br />
+      <label>
+        Corpo:
+        <textarea />
+      </label><br/>
+      <button onClick={() => { }}>Enviar</button>
+    </>
+  )
+}
+
+export default App
