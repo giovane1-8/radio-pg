c0939fde8e87ac1cbeab78b9377152c4068fabfe
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-01-24 16:05:41 -0300

    banner components styles

--- src/components/InitialBanner/index.tsx
+++ src/components/InitialBanner/index.tsx
@@ -3,10 +3,18 @@ import {
   Header,
   Image,
   NavigationWrapper,
+  ListItem,
+  ListItemGlow,
   Footer,
+  Button,
+  ButtonGlow
 } from './style'
 
 import logoFundoBranco from '../../utils/images/logoFundoBranco.png'
+import broadcast from '../../utils/icons/broadcast.svg'
+import communication from '../../utils/icons/communication.svg'
+import pause from '../../utils/icons/pause.svg'
+import playOne from '../../utils/icons/play-one.svg'
 
 export const InitialBanner = () => {
   return (
@@ -14,21 +22,19 @@ export const InitialBanner = () => {
       <Header>
         <Image src={logoFundoBranco} alt="Logo Fundo Branco" />
         <NavigationWrapper>
-          <ul>
-            <li>Sobre nós</li>
-            <li>As Melhores</li>
-            <li>Pedir uma música</li>
-            <li>Hall dos premiados</li>
-            <li>Parceiros</li>
-          </ul>
+          <ListItem href='#!'>Sobre nós</ListItem>
+          <ListItem href='#!'>As Melhores</ListItem>
+          <ListItem href='#!'>Pedir uma música</ListItem>
+          <ListItem href='#!'>Hall dos premiados</ListItem>
+          <ListItemGlow href='#!'>Parceiros</ListItemGlow>
         </NavigationWrapper>
       </Header>
 
       <Footer>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <ButtonGlow><img src={broadcast} /> AO VIVO</ButtonGlow>
+        <Button><img src={playOne} /></Button>
+        <Button><img src={pause} /></Button>
+        <Button><img src={communication} /></Button>
       </Footer>
     </Banner>
   )
--- src/components/InitialBanner/style.ts
+++ src/components/InitialBanner/style.ts
@@ -3,16 +3,87 @@ import styled from 'styled-components'
 export const Banner = styled.div`
   height: 90vh;
   background-color: ${props => props.theme.colors.lightGray};
+
+  position: relative;
 `
 
 export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 4rem 12.5rem 0;
 `
 
 export const Image = styled.img`
+  width: 8.75rem;
+  height: 5.625rem;
 `
 
-export const NavigationWrapper = styled.div`
+export const NavigationWrapper = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 55%;
+
+  list-style: none;
+  font-size: 24px;
+`
+
+export const ListItem = styled.a`
+  color: ${props => props.theme.colors.white};
+  font-weight: 500;
+
+  outline: none;
+  text-decoration: none;
+`
+
+export const ListItemGlow = styled.a`
+  color: ${props => props.theme.colors.primary};
+  font-weight: 700;
+
+  outline: none;
+  text-decoration: none;
 `
 
 export const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 0 12.5rem 5.5rem 0;
+`
+
+export const Button = styled.button`
+  padding: 10px;
+  border-radius: 80px;
+
+  border: 0;
+  margin: 0 12px 0;
+  
+  cursor: pointer;
+`
+
+export const ButtonGlow = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  
+  padding: 10px;
+  width: 150px;
+  
+  color: #FF4848;
+  font-size: 16px;
+  font-weight: 700;
+  
+  border-radius: 80px;
+  border: 0;
+  
+  cursor: pointer;
 `
