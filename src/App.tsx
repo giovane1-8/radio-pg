2fe317046226d2e938e58a98ef027d8649218afa
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-01-24 13:17:15 -0300

    default style configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router.routes'
 
+import GlobalStyle from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import _default from './styles/themes/default'
+
 export const App = () => {
   return (
-    <RouterProvider router={router} />
+    <ThemeProvider theme={_default}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'default',
+
+    colors: {
+    	primary: '#C9E483',
+			secondary: '#3C7B1E',
+	
+			white: '#FFFFFF',
+			lightGray: '#B3B0B8',
+			darkGray: '#7C7A7F',
+
+			black: '#000000',
+			black800: '#101114',
+			black600: '#1E1F24',
+			black400: '#2B2C33',
+			black200: '#454652',
+    }
+};
